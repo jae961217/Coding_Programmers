955dada91be10adb9e03f5c98b45d90ae766de10
jae961217 <jae961217@gmail.com>
2021-10-13 20:11:22 +0900

    구현

---
+++ py/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = []
+
+    numbers.sort()
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+
+    answer = list(set(answer))
+    answer.sort()
+    return answer
